267a32e97fc96b8ccf74dc5f20dbdaf33991a937
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-05-22 14:40:54 +0700

    Update Program.cs

--- ReadNumber/Program.cs
+++ ReadNumber/Program.cs
@@ -10,45 +10,157 @@ namespace ReadNumber
             Console.Write("Nhap so nguyen can doc: ");
             num = int.Parse(Console.ReadLine());
 
-            string resulst = "";
+            string result = "";
+            int hundreds = num / 100;
+            int tens = (num % 100) / 10;
             int ones = num % 10;
-            int tens = (num% 100)/10;
 
-            switch (ones)
+            switch (hundreds)
             {
                 case 1:
-                    resulst += "One";
+                    result += "one hundred";
                     break;
                 case 2:
-                    resulst += "two";
+                    result += "two hundred";
                     break;
                 case 3:
-                    resulst += "Three";
+                    result += "three hundred";
                     break;
                 case 4:
-                    resulst += "four";
+                    result += "four hundred";
                     break;
                 case 5:
-                    resulst += "Five";
+                    result += "five hundred";
                     break;
                 case 6:
-                    resulst += "Six";
+                    result += "six hundred";
                     break;
                 case 7:
-                    resulst += "Seven";
+                    result += "seven hundred";
                     break;
                 case 8:
-                    resulst += "Eight";
+                    result += "eight hundred";
                     break;
                 case 9:
-                    resulst += "Nine";
+                    result += "nine hundred";
                     break;
             }
-            if (num == 0 )
+
+            if (hundreds > 0 && (tens > 0 || ones > 0))
             {
-                resulst ="Zero";
+                result += " and ";
             }
-            Console.WriteLine(resulst);
+
+            switch (tens)
+            {
+                case 1:
+                    switch (ones)
+                    {
+                        case 0:
+                            result += "ten";
+                            break;
+                        case 1:
+                            result += "eleven";
+                            break;
+                        case 2:
+                            result += "twelve";
+                            break;
+                        case 3:
+                            result += "thirteen";
+                            break;
+                        case 4:
+                            result += "fourteen";
+                            break;
+                        case 5:
+                            result += "fifteen";
+                            break;
+                        case 6:
+                            result += "sixteen";
+                            break;
+                        case 7:
+                            result += "seventeen";
+                            break;
+                        case 8:
+                            result += "eighteen";
+                            break;
+                        case 9:
+                            result += "nineteen";
+                            break;
+                    }
+                    break;
+                case 2:
+                    result += "twenty";
+                    break;
+                case 3:
+                    result += "thirty";
+                    break;
+                case 4:
+                    result += "forty";
+                    break;
+                case 5:
+                    result += "fifty";
+                    break;
+                case 6:
+                    result += "sixty";
+                    break;
+                case 7:
+                    result += "seventy";
+                    break;
+                case 8:
+                    result += "eighty";
+                    break;
+                case 9:
+                    result += "ninety";
+                    break;
+            }
+
+            if (tens != 1 && ones > 0)
+            {
+                if (hundreds > 0 || tens > 0)
+                {
+                    result += " ";
+                }
+
+                switch (ones)
+                {
+                    case 1:
+                        result += "one";
+                        break;
+                    case 2:
+                        result += "two";
+                        break;
+                    case 3:
+                        result += "three";
+                        break;
+                    case 4:
+                        result += "four";
+                        break;
+                    case 5:
+                        result += "five";
+                        break;
+                    case 6:
+                        result += "six";
+                        break;
+                    case 7:
+                        result += "seven";
+                        break;
+                    case 8:
+                        result += "eight";
+                        break;
+                    case 9:
+                        result += "nine";
+                        break;
+                }
+            }
+            if (num == 0)
+            {
+                result = "zero";
+            }
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
         }
+        
     }
 }
